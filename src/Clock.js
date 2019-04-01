1ec1625ed03a51ed5f3eb12e588daa935465a5bb
Nakka LeelaPrasanth <44019459+leelanakka@users.noreply.github.com>
2019-04-01 14:46:03 +0530

    Modify Clock class

--- src/Clock.js
+++ src/Clock.js
@@ -1,15 +1,33 @@
 import React from "react";
-import ReactDOM from "react-dom";
 
-class Clock extends React.Component{
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
 
-    render(){
-      return (<div>
-      <p1> Hello My World!</p1> <br></br>
-      <p1> It is {this.props.date.toLocaleTimeString()}.</p1>
-    </div>)
-    }
+  componentDidMount() {
+    this.timerId = setInterval(() => {
+      this.tick();
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerId);
+  }
+
+  tick() {
+    this.setState({ date: new Date() });
+  }
+
+  render() {
+    return (
+      <div>
+        <p1> Hello My World!</p1> <br />
+        <p1> It is {this.state.date.toLocaleTimeString()}.</p1>
+      </div>
+    );
+  }
 }
-
 
 export default Clock;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Clock from "./Clock";
 
-function Tick() {
-  ReactDOM.render(<Clock date={new Date()} />, document.getElementById("root"));
-}
 
-setInterval(Tick, 1000);
+ReactDOM.render(<Clock />, document.getElementById("root"));
