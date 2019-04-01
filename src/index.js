2496b8e4ccb39f19d9f54d85e8e97753234f869a
Nakka LeelaPrasanth <44019459+leelanakka@users.noreply.github.com>
2019-04-01 14:03:01 +0530

    tried to serve photos

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Welcome(props) {
+  return (
+    <div>
+      <h1>Welcome {props.name}</h1>
+    </div>
+  );
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function App() {
+  return (
+    <div>
+      <Welcome name="Nakka" />
+      <Welcome name="Leela" />
+      <Welcome name="prasanth" />
+      <Welcome name="It does Work" />
+      <img
+        src="./images/image-4f5952b3351e98e47db3ac011017000a.png"
+        alt="something"
+      />
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
