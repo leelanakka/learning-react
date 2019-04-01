2263610495cb04a12810d22564be7da8540fae6e
Nakka LeelaPrasanth <44019459+leelanakka@users.noreply.github.com>
2019-04-01 14:21:59 +0530

    add clock function and using that in tick function and rendering that in tick

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-function Tick() {
-  const element = (
+
+function Clock(props){
+  return (
     <div>
       <p1> Hello My World!</p1> <br></br>
-      <p1> It is {new Date().toLocaleTimeString()}.</p1>
+      <p1> It is {props.date.toLocaleTimeString()}.</p1>
     </div>
-  );
-  ReactDOM.render(element, document.getElementById("root"));
+  )
+}
+
+function Tick() {
+  ReactDOM.render(<Clock date={new Date()}  />, document.getElementById("root"));
 }
 
 setInterval(Tick,1000)
